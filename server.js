0284f51917f28f392858267dd37fb48eb607393a
VIDYUT4 <vidyutbiswas4@gmail.com>
2024-09-26 18:38:49 +0530

    test_commit

--- server.js
+++ server.js
@@ -2,5 +2,6 @@ var app = require('./app');
 var port = process.env.PORT || 8080;
 
 var server = app.listen(port, function() {
+  console.log('Successful to connect Mongo')
   console.log('Express server listening on port ' + port);
 });
